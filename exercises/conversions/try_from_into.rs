0408ea7557526b1f414aef457d308bfdaaf6444d
mcksn <mackson.ogu@gmail.com>
2022-01-02 16:33:10 +0000

    switching to desktop..

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -21,7 +21,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
@@ -32,10 +31,17 @@ enum IntoColorError {
 // but the slice implementation needs to check the slice length!
 // Also note that correct RGB color values must be integers in the 0..=255 range.
 
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+
+        Ok(Color{
+             red:tuple.0.try_into().map_err(|e| IntoColorError::IntConversion)?,
+            green:tuple.1.try_into().map_err(|e| IntoColorError::IntConversion)?,
+            blue:tuple.2.try_into().map_err(|e| IntoColorError::IntConversion)?
+        })
     }
 }
 
@@ -43,6 +49,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Ok(Color{
+            red:arr[0].try_into().map_err(|e| IntoColorError::IntConversion)?,
+            green:arr[1].try_into().map_err(|e| IntoColorError::IntConversion)?,
+            blue:arr[2].try_into().map_err(|e| IntoColorError::IntConversion)?
+            })
     }
 }
 
@@ -50,8 +61,27 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
-    }
-}
+
+       if slice.len() > 3 {
+           return Err(IntoColorError::BadLen);
+       }
+       let mut iterator = slice.iter();  
+       Ok(Color{
+       red: match iterator.next() {
+       None => return Err(IntoColorError::BadLen),
+       Some(x) => (*x).try_into().map_err(|e| IntoColorError::IntConversion)? 
+       },
+       green: match iterator.next() {
+       None => return Err(IntoColorError::BadLen),
+       Some(x) => (*x).try_into().map_err(|e| IntoColorError::IntConversion)?
+       },
+       blue: match iterator.next() {
+       None => return Err(IntoColorError::BadLen),
+       Some(x) => (*x).try_into().map_err(|e| IntoColorError::IntConversion)?
+       }
+    })
+
+    }}
 
 fn main() {
     // Use the `from` function
